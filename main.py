0f8ab78eed9e85abd1489b467baeb9ee5bd3f5f0
wez3 <wez3@users.noreply.github.com>
2019-09-04 20:06:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ print("AppKey: %s" % (binascii.hexlify(app_key)))
 
 # join a network using OTAA (Over the Air Activation)
 if not lora.has_joined():
-    lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
+    lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=10)
 
 # wait until the module has joined the network
 while not lora.has_joined():
@@ -45,55 +45,56 @@ while not lora.has_joined():
     pycom.rgbled(0x000000)
     time.sleep(1.0)
     print('Not yet joined...')
+    print('OTAA joined')
 
 if lora.has_joined():
-    pycom.rgbled(0x001400)
-
     # create a LoRa socket
     s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
     # set the LoRaWAN data rate
     s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
 
-    s.setblocking(True)
-    pycom.rgbled(0x000014)
-    lpp = CayenneLPP()
+    s.settimeout(180.0)
+    try:
+        pycom.rgbled(0x000014)
+        lpp = CayenneLPP()
 
-    print('\n\n** 3-Axis Accelerometer (LIS2HH12)')
-    print('Acceleration', li.acceleration())
-    print('Roll', li.roll())
-    print('Pitch', li.pitch())
-    lpp.add_accelerometer(1, li.acceleration()[0], li.acceleration()[1], li.acceleration()[2])
-    lpp.add_gryrometer(1, li.roll(), li.pitch(), 0)
+        print('\n\n** 3-Axis Accelerometer (LIS2HH12)')
+        print('Acceleration', li.acceleration())
+        print('Roll', li.roll())
+        print('Pitch', li.pitch())
+        lpp.add_accelerometer(1, li.acceleration()[0], li.acceleration()[1], li.acceleration()[2])
+        lpp.add_gryrometer(1, li.roll(), li.pitch(), 0)
 
-    print('\n\n** Digital Ambient Light Sensor (LTR-329ALS-01)')
-    print('Light', lt.light())
-    lpp.add_luminosity(1, lt.light()[0])
-    lpp.add_luminosity(2, lt.light()[1])
+        print('\n\n** Digital Ambient Light Sensor (LTR-329ALS-01)')
+        print('Light', lt.light())
+        lpp.add_luminosity(1, lt.light()[0])
+        lpp.add_luminosity(2, lt.light()[1])
 
-    print('\n\n** Humidity and Temperature Sensor (SI7006A20)')
-    print('Humidity', si.humidity())
-    print('Temperature', si.temperature())
-    lpp.add_relative_humidity(1, si.humidity())
-    lpp.add_temperature(1, si.temperature())
+        print('\n\n** Humidity and Temperature Sensor (SI7006A20)')
+        print('Humidity', si.humidity())
+        print('Temperature', si.temperature())
+        lpp.add_relative_humidity(1, si.humidity())
+        lpp.add_temperature(1, si.temperature())
 
-    mpPress = MPL3115A2(py,mode=PRESSURE)
-    print('\n\n** Barometric Pressure Sensor with Altimeter (MPL3115A2)')
-    print('Pressure (hPa)', mpPress.pressure()/100)
-    lpp.add_barometric_pressure(1, mpPress.pressure()/100)
+        mpPress = MPL3115A2(py,mode=PRESSURE)
+        print('\n\n** Barometric Pressure Sensor with Altimeter (MPL3115A2)')
+        print('Pressure (hPa)', mpPress.pressure()/100)
+        lpp.add_barometric_pressure(1, mpPress.pressure()/100)
 
-    mpAlt = MPL3115A2(py,mode=ALTITUDE)
-    print('Altitude', mpAlt.altitude())
-    print('Temperature', mpAlt.temperature())
-    lpp.add_gps(1, 0, 0, mpAlt.altitude())
-    lpp.add_temperature(2, mpAlt.temperature())
+        mpAlt = MPL3115A2(py,mode=ALTITUDE)
+        print('Altitude', mpAlt.altitude())
+        print('Temperature', mpAlt.temperature())
+        lpp.add_gps(1, 0, 0, mpAlt.altitude())
+        lpp.add_temperature(2, mpAlt.temperature())
 
-    print('Sending data (uplink)...')
-    s.send(bytes(lpp.get_buffer()))
-    s.setblocking(False)
-    data = s.recv(64)
-    print('Received data (downlink)', data)
-    pycom.rgbled(0x001400)
+        print('Sending data (uplink)...')
+        s.send(bytes(lpp.get_buffer()))
+        s.setblocking(False)
+        data = s.recv(64)
+        print('Received data (downlink)', data)
+    except:
+        1
 
 lora.nvram_save()
 
